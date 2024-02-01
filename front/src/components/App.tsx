8d28f3c1ba2d99d308d6ba3dbf26e6583dc89636
RobinS <robin.le.dev@gmail.com>
2024-02-01 18:05:55 +0100

    Ajout de landingPage pour pouvoir coller le footer en bas

--- front/src/components/App.tsx
+++ front/src/components/App.tsx
@@ -3,12 +3,15 @@ import './App.scss'
 import Footer from './footer/Footer'
 import Header from './header/Header'
 import Navbar from './navbar/Navbar'
+import LandingPage from "./landingPage/LandingPage.tsx";
 function App() {
 
   return (
     <div id="app">
       <Header />
       <Navbar />
+      <LandingPage />
+
       <Footer />
     </div>
   )
---
+++ front/src/components/landingPage/LandingPage.tsx
@@ -0,0 +1,10 @@
+import "./LandingPage.scss"
+function LandingPage(){
+    return(
+        <main>
+            <h1>Contenue</h1>
+        </main>
+    )
+}
+
+export default LandingPage;
