16e9a6b9cb7a32f3b780dbfab2e22c77e37e443a
Robin Simonneau <robin.simmoneau@eviden.com>
2024-02-16 11:15:54 +0100

    Ajout de la pages de bulbes

--- front/src/components/bulbesPage/BublesPage.tsx
+++ front/src/components/bulbesPage/BublesPage.tsx
@@ -4,6 +4,7 @@ import ProductItem from "../ProductItem/ProductItem.tsx";
 function BublesPage(){
     return(
         <main>
+            <h1>Les bulbes</h1>
             <ProductItem refer="b01" />
             <ProductItem refer={"b02"} />
             <ProductItem refer={"b03"} />
