4318601432d886288d7ae231b0e5cfb4dd7b3958
Robin Simonneau <robin.simmoneau@eviden.com>
2024-02-16 11:21:10 +0100

    Modification du css

--- front/src/components/App.tsx
+++ front/src/components/App.tsx
@@ -5,6 +5,8 @@ import Header from './header/Header'
 import LandingPage from "./landingPage/LandingPage.tsx";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import BublesPage from "./bulbesPage/BublesPage.tsx";
+import PlantesAMassif from "./plantesAMassif/PlantesAMassif.tsx";
+import Rosiers from "./rosiers/Rosiers.tsx";
 
 function App() {
 
@@ -15,6 +17,8 @@ function App() {
               <Routes>
                   <Route path="/" element={<LandingPage/>}/>
                   <Route path="/bulbes" element={<BublesPage/>}/>
+                  <Route path="/plantes-a-massif" element={<PlantesAMassif/>}/>
+                  <Route path="/rosiers" element={<Rosiers/>}/>
               </Routes>
 
               <Footer/>
