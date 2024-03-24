59291723bd1a4c5883b3a3110a3483f5b55da5d1
RobinS <robin.le.dev@gmail.com>
2024-03-24 12:59:42 +0100

    Ajout au panel admin la possibilité d'ajouter/enlever les données des models

--- back/RestAPI/admin.py
+++ back/RestAPI/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from RestAPI.models import Flower, Basket
 # Register your models here.
+
+class FlowerAdmin(admin.ModelAdmin):
+    list_display = ('id','name','price','stock')
+
+class BasketAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'display_flowers')
+    def display_flowers(self, obj):
+        return ', '.join([flower.name for flower in obj.flowers.all()])
+
+
+
+admin.site.register(Flower,FlowerAdmin)
+admin.site.register(Basket,BasketAdmin)
+
+
