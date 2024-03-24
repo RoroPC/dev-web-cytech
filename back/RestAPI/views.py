30b0097e188f211c134602952ddcd3c40416c857
RobinS <robin.le.dev@gmail.com>
2024-03-24 15:51:26 +0100

    Ajout de la possibilité de modifier le stock de fleurs

--- back/RestAPI/views.py
+++ back/RestAPI/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 
 from .models import Flower, Basket, Category
 from django.contrib.auth.models import User
@@ -10,22 +10,68 @@ from rest_framework.views import APIView
 
 
 class FlowerListView(APIView):
-
+    """
+        Get list of all the flowers
+    """
     def get(self, request, format=None):
         queryset = Flower.objects.all()
         serializer_class = FlowerSerializer(queryset, many=True)
         return Response(serializer_class.data)
 
 
+class FlowerDetailView(APIView):
+    """
+        Get flowers details
+    """
+    def get(self, request, flower_id, format=None):
+        try:
+            Flower.objects.get(id=flower_id)
+        except Flower.DoesNotExist:
+            return Response({"error": "Flower not found"}, status=status.HTTP_404_NOT_FOUND)
+        queryset = Flower.objects.filter(id=flower_id)
+        serializer = FlowerSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def patch(self, request, flower_id, format=None):
+        try:
+            flower = Flower.objects.get(id=flower_id)
+        except Flower.DoesNotExist:
+            return Response({"error": "Flower not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        new_stock = request.data.get('stock')
+
+        if new_stock is None:
+            return Response({"error": "New stock is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if new_stock < 0:
+            return Response({"error": "New stock should be superior than 0"}, status=status.HTTP_400_BAD_REQUEST)
+
+        flower.stock = new_stock
+        flower.save()
+
+        serializer = FlowerSerializer(flower)
+        return Response(serializer.data)
+
+
 class FlowerListViewByCategory(generics.ListAPIView):
+    """
+        Get flowers from a category
+    """
     serializer_class = FlowerSerializer
 
     def get_queryset(self):
         category_id = self.kwargs['category_id']
+        try:
+            Category.objects.get(id=category_id)
+        except Category.DoesNotExist:
+            return Response({"error": "Category not found"}, status=status.HTTP_404_NOT_FOUND)
         return Flower.objects.filter(category_id=category_id)
 
 
 class CategoryListView(APIView):
+    """
+        Get List of all categories
+    """
     def get(self, request):
         queryset = Category.objects.all()
         serializer_class = CategorySerializer(queryset, many=True)
@@ -33,6 +79,9 @@ class CategoryListView(APIView):
 
 
 class UserDetailView(APIView):
+    """
+        Get users details
+    """
     def get(self, request, format=None):
         data = {
             "username": request.user.username,
@@ -44,6 +93,9 @@ class UserDetailView(APIView):
 
 
 class BasketDetailView(APIView):
+    """
+        Get current user basket
+    """
     def get(self, request, format=None):
         current_user = request.user.id
         queryset = Basket.objects.filter(user=current_user)
