6203d616f79753b5ac1e2b6ca34e6c972f4fef58
Robin Simonneau <robin.simmoneau@eviden.com>
2024-04-09 10:46:26 +0200

    Correction d'un bug qui demandait d'être connecté pour voir les fleurs

--- back/back/settings.py
+++ back/back/settings.py
@@ -38,16 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'RestAPI'
+    'RestAPI',
+    'corsheaders'
+
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
 ]
-CORS_ALLOWED_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'back.urls'
