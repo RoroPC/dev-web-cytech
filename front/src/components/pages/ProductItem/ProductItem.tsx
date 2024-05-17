e47bf076f1b1e5e701483b773c3b4b764411f092
robis <robin.simonneau@etudiant.univ-reims.fr>
2024-05-17 20:08:14 +0200

    Ajout des requetes pour récupérer les plantes

--- front/src/components/pages/ProductItem/ProductItem.tsx
+++ front/src/components/pages/ProductItem/ProductItem.tsx
@@ -4,15 +4,15 @@ import ProductItemType from "./ProductItemType.ts"
 import PropTypes from "prop-types";
 import {useState} from "react";
 
-function ProductItem({refer, title, img, price, stock}:ProductItemType) {
+function ProductItem({id, name, img, price, stock}:ProductItemType) {
     const [seeStock, setSeeStock] = useState(false);
     const [qtOrder, setqtOrder] = useState(0);
     return (
-        <div className="product-item" id={refer}>
+        <div className="product-item" id={id}>
             <div className="product-item__image">
                 <img src={img} alt="product"/>
             </div>
-            <div className="product-item__info__title">{title}</div>
+            <div className="product-item__info__title">{name}</div>
             <div className="product-item__info__price">{price} $</div>
             {seeStock && (
                 <div className="product-item__info__stock--visible">{stock}</div>
@@ -41,14 +41,14 @@ function ProductItem({refer, title, img, price, stock}:ProductItemType) {
 }
 
 ProductItem.propTypes = {
-    refer: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
     img: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired
+    price: PropTypes.string.isRequired
 }
 
 ProductItem.defaultProps = {
-    title: "Product Title",
+    name: "Product Title",
     img: "https://placehold.co/400",
     price: 50,
     stock: 20
