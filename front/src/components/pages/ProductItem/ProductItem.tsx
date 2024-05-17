b944a9f7fb656f0c3597065d06f21aa0d8c82e70
robis <robin.simonneau@etudiant.univ-reims.fr>
2024-05-17 20:24:50 +0200

    Ajout de la diminution du stocks si on clique sur ajouter au panier

--- front/src/components/pages/ProductItem/ProductItem.tsx
+++ front/src/components/pages/ProductItem/ProductItem.tsx
@@ -3,10 +3,12 @@ import "./ProductItem.scss";
 import ProductItemType from "./ProductItemType.ts"
 import PropTypes from "prop-types";
 import {useState} from "react";
+import {BASE_URL} from "../../../services/api/api.ts";
 
 function ProductItem({id, name, img, price, stock}:ProductItemType) {
     const [seeStock, setSeeStock] = useState(false);
     const [qtOrder, setqtOrder] = useState(0);
+    const [stockState, setStockState] = useState(stock)
     return (
         <div className="product-item" id={id}>
             <div className="product-item__image">
@@ -15,7 +17,7 @@ function ProductItem({id, name, img, price, stock}:ProductItemType) {
             <div className="product-item__info__title">{name}</div>
             <div className="product-item__info__price">{price} $</div>
             {seeStock && (
-                <div className="product-item__info__stock--visible">{stock}</div>
+                <div className="product-item__info__stock--visible">{stockState}</div>
             )}
             <button onClick={() => setSeeStock(!seeStock)}>Stock</button>
 
@@ -35,7 +37,23 @@ function ProductItem({id, name, img, price, stock}:ProductItemType) {
             }}>+
             </button>
 
-            <button>Ajouter Panier</button>
+            <button onClick={()=>{
+                const newStock = stock- qtOrder;
+                const stockData = {stock:newStock};
+                fetch(BASE_URL+"/flowers/"+id+"/",
+                    {
+                        method:'PATCH',
+                        headers:{
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(stockData),
+                        credentials: "include"
+                }).then(response => {
+                    if(!response.ok) console.log("Error while updating stock ! "+response.status)
+                    else setStockState(newStock);
+                }).catch(error=>(console.log("Error while updating the stock : "+error)));
+
+            }}>Ajouter Panier</button>
         </div>
     );
 }
