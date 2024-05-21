01ff0db69359da767c714724416415cafee0edd0
robis <robin.simonneau@etudiant.univ-reims.fr>
2024-05-21 19:13:02 +0200

    Ajout d'un type a imageSrc

--- front/src/components/pages/ProductItem/ProductItem.tsx
+++ front/src/components/pages/ProductItem/ProductItem.tsx
@@ -11,7 +11,7 @@ function ProductItem({refer, title, img, price, stock}:ProductItemType) {
     const [isOpen, setIsOpen] = useState(false);
     const [currentImg, setCurrentImg] = useState('');
 
-    const openPopup = (imgSrc) => {
+    const openPopup = (imgSrc: string) => {
         setCurrentImg(imgSrc);
         setIsOpen(true);
     };
